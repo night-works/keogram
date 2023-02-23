1c34d09097ff52b7ceefeb16e915929940a6d1e8
Joseph McCarthy <joseph-mccarthy@users.noreply.github.com>
2023-02-23 20:56:54 +0000

    init structure

---
+++ keogram/dummy.py
@@ -0,0 +1,3 @@
+
+def coverage_method():
+    return 10
---
+++ tests/test_dummy.py
@@ -0,0 +1,9 @@
+from keogram import coverage_method
+
+def test_coverage():
+    value = coverage_method()
+    assert value == 10
+
+
+def test_something():
+    assert 10 == 10
