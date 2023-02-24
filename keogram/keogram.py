f53871d806ba39c808788970949d1b4372b4942d
Joseph McCarthy <joseph-mccarthy@users.noreply.github.com>
2023-02-24 12:29:59 +0000

    reactor valid image types

--- keogram/keogram.py
+++ keogram/keogram.py
@@ -4,6 +4,8 @@ import argparse
 
 from PIL import Image
 
+valid_images = [".jpg", ".gif", ".png", ".tga"]
+
 
 def create(source: Union[str, PathLike], destination: Union[str, PathLike], keogram_file: str = "keogram.jpg") -> None:
     if path.exists(source):
@@ -18,21 +20,22 @@ def create(source: Union[str, PathLike], destination: Union[str, PathLike], keog
 
 
 def process_images(source, destination, file_name):
-    p: Image = Image.new("RGB", (0, 0))
-    valid_images = [".jpg", ".gif", ".png", ".tga"]
-    image_number = 1
-    for f in sorted(listdir(source)):
-        ext = path.splitext(f)[1]
-        if ext.lower() not in valid_images:
+    keogram = Image.new("RGB", (0, 0))
+
+    for file_item in sorted(listdir(source)):
+        if not valid_image(file_item):
             continue
-        image_open = Image.open(path.join(source, f))
+        image_open = Image.open(path.join(source, file_item))
         image_middle = int(image_open.width / 2)
         crop_rectangle = (image_middle, 0, image_middle + 1, image_open.height)
         image_open = image_open.crop(crop_rectangle)
-        print(f"Processing Image {image_number}")
-        image_number += 1
-        p = concat_images(p, image_open)
-    p.save(f"{destination}/{file_name}")
+        keogram = concat_images(keogram, image_open)
+    keogram.save(f"{destination}/{file_name}")
+
+
+def valid_image(file_item) -> bool:
+    ext = path.splitext(file_item)[1]
+    return ext.lower() in valid_images
 
 
 def concat_images(im1, im2):
@@ -52,4 +55,3 @@ if __name__ == '__main__':
         create(args.source, args.destination)
     except OSError as e:
         print(e)
-
