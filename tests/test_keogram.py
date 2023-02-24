a919cd8213f9c7442122364bc408a868dcbba842
Joseph McCarthy <joseph-mccarthy@users.noreply.github.com>
2023-02-24 12:42:45 +0000

    adding tests for the concat of two images

--- tests/test_keogram.py
+++ tests/test_keogram.py
@@ -1,5 +1,6 @@
-from keogram.keogram import valid_image
+from keogram.keogram import valid_image, concat_images
 import pytest
+from PIL import Image
 
 
 @pytest.mark.parametrize("valid_format", ["jpg", "gif", "png", "jpeg"])
@@ -10,3 +11,11 @@ def test_valid_image_formats(valid_format):
 @pytest.mark.parametrize("invalid_format", ["tif", "dng", "raw"])
 def test_invalid_image_formats(invalid_format):
     assert not valid_image(f"/something/test.{invalid_format}")
+
+
+def test_concat_images():
+    first = Image.new("RGB", (10, 10))
+    second = Image.new("RGB", (15, 10))
+    result = concat_images(first, second)
+    assert result.width == 25
+    assert result.height == 10
